79a6e6042fdbc469fd5e703780d622ff6a457417
Øystein Grande <oystgran@stud.ntnu.no>
2025-03-07 14:04:27 +0100

    Added archive to archive screen and book to bookscreen

--- campingboka/src/screens/ArchiveScreen.vue
+++ campingboka/src/screens/ArchiveScreen.vue
@@ -1,29 +1,42 @@
 <template>
-    <div class="archive-screen">
-        <h1>Arkiv</h1>
-        <p>Campingboka</p>
-    </div>
+  <div class="archive-screen">
+    <h1>Arkiv</h1>
+    <ArchiveTable></ArchiveTable>
+  </div>
 </template>
 
 <script>
+import ArchiveTable from "@/components/ArchiveTable.vue";
 export default {
-    name: 'ArchiveScreen',
+  name: "ArchiveScreen",
+  components: {
+    ArchiveTable,
+  },
 };
 </script>
 
 <style scoped>
 .archive-screen {
-    text-align: center;
-    margin-top: 50px;
+  text-align: center;
+  margin-top: 50px;
+  width: 800px; /* 80% av viewport width */
+  max-width: 1200px; /* Valgfritt: setter en maks bredde */
+  height: 100vh; /* Sikrer at den fyller høyden av skjermen */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-left: auto;
+  margin-right: auto;
 }
 
 .archive-screen h1 {
-    font-size: 2.5em;
-    color: #4CAF50;
+  font-size: 2.5em;
+  color: #4caf50;
 }
 
 .archive-screen p {
-    font-size: 1.2em;
-    color: #555;
+  font-size: 1.2em;
+  color: #555;
 }
 </style>
--- campingboka/src/screens/BookScreen.vue
+++ campingboka/src/screens/BookScreen.vue
@@ -1,47 +1,35 @@
 <template>
-    <div class="book-screen">
-        <h1>Book</h1>
-        <p>Campingboka</p>
-        <!-- <AgGridTable></AgGridTable> -->
-        <ArchiveTable></ArchiveTable>
-    </div>
+  <div class="book-screen">
+    <h1>Book</h1>
+    <p>Campingboka</p>
+    <AgGridTable></AgGridTable>
+  </div>
 </template>
 
 <script>
-/* import AgGridTable from '@/components/AgGridTable.vue'; */
-import ArchiveTable from '@/components/ArchiveTable.vue';
+import AgGridTable from "@/components/AgGridTable.vue";
 
 export default {
-    name: 'BookScreen',
-    components: {
-        /* AgGridTable */
-        ArchiveTable, // Add it here
-    },
+  name: "BookScreen",
+  components: {
+    AgGridTable, // Add it here
+  },
 };
 </script>
 
 <style scoped>
 .book-screen {
-    text-align: center;
-    margin-top: 50px;
-    width: 800px; /* 80% av viewport width */
-    max-width: 1200px; /* Valgfritt: setter en maks bredde */
-    height: 100vh; /* Sikrer at den fyller høyden av skjermen */
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    margin-left: auto;
-    margin-right: auto;
+  text-align: center;
+  margin-top: 50px;
 }
 
 .book-screen h1 {
-    font-size: 2.5em;
-    color: #4CAF50;
+  font-size: 2.5em;
+  color: #4caf50;
 }
 
 .book-screen p {
-    font-size: 1.2em;
-    color: #555;
+  font-size: 1.2em;
+  color: #555;
 }
 </style>
