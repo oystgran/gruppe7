cd2460bb030ed31d6cf4791eea4d8bfa06458ea8
hnrkljnsn <153367107+hnrkljnsn@users.noreply.github.com>
2025-03-24 10:50:55 +0100

    Added emitters to each rectangle in the svg map. Now each rectangle emits its respective number.

--- campingboka/src/components/MapComponent.vue
+++ campingboka/src/components/MapComponent.vue
@@ -7,48 +7,48 @@
   <path id="svg_2" stroke="black" stroke-width="5" d="m4862.44186,8681.37209c-205,-10 -765,-89 -941,-133c-45,-11 -144,-27 -221,-37c-157,-19 -130,-21 -563,48c-462,74 -880,105 -1060,80c-341,-48 -743,-233 -1085,-498c-131,-102 -346,-304 -415,-390c-96,-119 -223,-414 -234,-543c-15,-173 -13,-186 43,-326c139,-345 383,-687 587,-824c156,-104 257,-118 494,-71c255,51 352,41 408,-40c39,-56 28,-128 -47,-292c-68,-150 -86,-218 -78,-289c14,-112 126,-248 266,-322c169,-89 302,-89 595,-2c172,51 302,29 496,-86c117,-69 199,-130 359,-267c265,-227 352,-297 523,-422c270,-198 451,-348 503,-419c72,-99 67,-159 -27,-321c-62,-108 -92,-194 -82,-238c24,-99 89,-150 386,-304c123,-63 257,-134 298,-157c212,-118 350,-168 650,-236c227,-51 340,-88 515,-167c201,-90 840,-402 1010,-493c432,-230 995,-491 1420,-659c149,-58 362,-144 475,-190c373,-153 540,-209 778,-259c324,-68 474,-81 952,-81c430,1 565,9 980,61c465,57 742,119 1035,229c151,57 278,124 520,275c278,173 353,216 500,286c182,87 374,145 670,204c422,84 621,141 798,230c225,113 348,257 394,460c26,114 23,441 -5,573c-56,265 -212,440 -568,636c-219,120 -395,189 -729,286c-255,74 -359,113 -478,183c-87,49 -235,156 -357,257c-87,71 -210,155 -360,245c-251,151 -446,290 -740,530c-85,69 -204,162 -265,205c-60,44 -190,139 -289,213c-290,218 -347,247 -801,402c-115,39 -322,118 -460,175c-137,56 -324,131 -415,165c-233,88 -516,215 -715,320c-243,129 -666,342 -830,417c-77,36 -232,108 -345,160c-247,113 -377,182 -530,279c-151,95 -434,294 -585,409c-169,130 -275,197 -428,270c-144,69 -609,249 -807,314c-382,124 -779,175 -1195,154zm545,-38c344,-38 568,-98 1034,-277c468,-179 530,-212 845,-447c486,-364 705,-496 1186,-716c211,-96 827,-401 1078,-533c220,-116 498,-239 803,-356c120,-46 309,-122 419,-168c110,-47 308,-122 440,-168c396,-137 468,-172 715,-356c85,-64 223,-164 305,-224c83,-59 211,-158 285,-220c266,-221 575,-438 845,-594c46,-27 171,-116 277,-199c357,-278 431,-315 856,-437c286,-82 496,-166 715,-287c239,-132 394,-276 472,-437c65,-134 78,-211 79,-461c0,-228 -9,-294 -59,-398c-75,-159 -241,-286 -496,-381c-147,-54 -291,-92 -555,-146c-295,-60 -416,-90 -536,-134c-194,-70 -366,-157 -608,-306c-74,-46 -181,-112 -237,-146c-419,-259 -880,-378 -1818,-471c-261,-25 -948,-25 -1155,0c-361,45 -655,123 -1012,268c-95,39 -276,111 -403,160c-126,50 -288,113 -360,141c-190,75 -709,314 -940,432c-110,57 -301,155 -425,219c-274,141 -866,423 -1010,482c-153,62 -213,80 -435,130c-305,70 -366,93 -695,269c-91,49 -230,122 -310,163c-165,86 -214,118 -257,167c-71,80 -65,134 29,294c88,150 102,198 77,273c-32,94 -186,235 -543,496c-217,159 -249,185 -471,378c-186,161 -258,218 -360,285c-116,75 -233,129 -321,147c-92,19 -148,12 -335,-43c-154,-44 -264,-53 -363,-30c-150,35 -300,145 -357,261c-29,59 -31,70 -27,142c4,80 9,95 94,283c35,77 39,93 39,168l0,83l-41,39c-74,69 -156,74 -398,24c-263,-53 -402,-21 -573,131c-179,159 -401,500 -508,779l-38,99l7,130c7,121 11,138 48,231c22,56 63,148 91,205c44,90 63,117 151,209c437,455 885,719 1380,813c28,5 139,10 246,10c239,1 492,-24 890,-87c157,-25 312,-48 345,-51c65,-6 291,19 395,43c147,35 574,98 860,127c103,11 518,5 640,-8z"/>
  </g>
  
- <rect transform="rotate(-15 1467.38 767.905)" rx="5" id="svg_3" height="60" width="40" y="737.90477" x="1447.38093" stroke="#FFAC8D" fill="#FFAC8D"/>
- <rect transform="rotate(-15 1413.95 779.81)" rx="5" id="svg_4" height="60" width="40" y="749.80953" x="1393.95235" stroke="#FFAC8D" fill="#FFAC8D"/>
- <rect transform="rotate(-20 1516.43 751.815)" rx="5" id="svg_5" height="60" width="40" y="721.81454" x="1496.43356" stroke="#FFAC8D" fill="#FFAC8D"/>
- <rect transform="rotate(-4 1105.1 876.857)" rx="5" id="svg_6" height="60" width="40" y="846.85716" x="1085.09521" stroke="#FFAC8D" fill="#FFAC8D"/>
- <rect transform="rotate(-12 1214.3 864.988)" rx="5" id="svg_7" height="60" width="40" y="834.98818" x="1194.30359" stroke="#FFAC8D" fill="#FFAC8D"/>
- <rect transform="rotate(20 845.762 813.238)" rx="5" id="svg_8" height="60" width="40" y="783.2381" x="825.76191" stroke="#FFAC8D" fill="#FFAC8D"/>
- <rect transform="rotate(-20 1266.59 851.149)" rx="5" id="svg_9" height="60" width="40" y="821.14893" x="1246.5893" stroke="#FFAC8D" fill="#FFAC8D"/>
- <rect rx="5" id="svg_10" height="60" width="40" y="845.42858" x="1027.47617" stroke="#FFAC8D" fill="#FFAC8D"/>
- <rect transform="rotate(7 996.714 865.714)" rx="5" id="svg_11" height="60" width="40" y="835.71429" x="976.71426" stroke="#FFAC8D" fill="#FFAC8D"/>
- <rect transform="rotate(-24 1363.63 801.06)" rx="5" id="svg_12" height="60" width="40" y="771.05975" x="1343.63085" stroke="#FFAC8D" fill="#FFAC8D"/>
- <rect transform="rotate(-28 1315.94 827.269)" rx="5" id="svg_13" height="60" width="40" y="797.26882" x="1295.94007" stroke="#FFAC8D" fill="#FFAC8D"/>
- <rect transform="rotate(-7 1160.05 872.798)" rx="5" id="svg_14" height="60" width="40" y="842.79771" x="1140.04758" stroke="#FFAC8D" fill="#FFAC8D"/>
- <rect transform="rotate(27 742.524 763.524)" rx="5" id="svg_15" height="60" width="40" y="733.52381" x="722.52377" stroke="#FFAC8D" fill="#FFAC8D"/>
- <rect transform="rotate(28 694.524 736.667)" rx="5" id="svg_16" height="60" width="40" y="706.66667" x="674.52376" stroke="#FFAC8D" fill="#FFAC8D"/>
- <rect transform="rotate(40 257.4 443.439)" rx="5" id="svg_17" height="60" width="40" y="413.43862" x="237.39991" stroke="#FFAC8D" fill="#FFAC8D"/>
- <rect transform="rotate(15 585.762 692.857)" rx="5" id="svg_18" height="60" width="40" y="662.85716" x="565.76188" stroke="#FFAC8D" fill="#FFAC8D"/>
- <rect transform="rotate(14 946.048 850.667)" rx="5" id="svg_19" height="60" width="40" y="820.66669" x="926.04757" stroke="#FFAC8D" fill="#FFAC8D"/>
- <rect transform="rotate(25 640.81 711.048)" rx="5" id="svg_20" height="60" width="40" y="681.04762" x="620.80949" stroke="#FFAC8D" fill="#FFAC8D"/>
- <rect transform="rotate(20 896.619 833.333)" rx="5" id="svg_21" height="60" width="40" y="803.33335" x="876.61901" stroke="#FFAC8D" fill="#FFAC8D"/>
- <rect transform="rotate(50 122.582 351.17)" rx="5" id="svg_22" height="60" width="35" y="321.1696" x="105.08185" stroke="#FFAC8D" fill="#FFAC8D"/>
- <rect transform="rotate(24 791.952 788.857)" rx="5" id="svg_23" height="60" width="40" y="758.85715" x="771.95238" stroke="#FFAC8D" fill="#FFAC8D"/>
- <rect transform="rotate(40 221.225 414.105)" rx="5" id="svg_24" height="60" width="40" y="384.10528" x="201.22451" stroke="#FFAC8D" fill="#FFAC8D"/>
- <rect transform="rotate(60 71.6432 276.596)" rx="5" id="svg_25" height="60" width="40" y="246.5965" x="51.64324" stroke="#FFAC8D" fill="#FFAC8D"/>
- <rect transform="rotate(55 90.7192 319.801)" rx="5" id="svg_26" height="60" width="40" y="289.80118" x="70.71925" stroke="#FFAC8D" fill="#FFAC8D"/>
- <rect transform="rotate(-20 1489.29 673.243)" rx="5" id="svg_27" height="60" width="40" y="643.24311" x="1469.2907" stroke="#FFAC8D" fill="#FFAC8D"/>
- <rect transform="rotate(-15 1440.24 686.476)" rx="5" id="svg_28" height="60" width="40" y="656.47619" x="1420.23807" stroke="#FFAC8D" fill="#FFAC8D"/>
- <rect transform="rotate(-15 1386.81 702.667)" rx="5" id="svg_29" height="60" width="40" y="672.66667" x="1366.8095" stroke="#FFAC8D" fill="#FFAC8D"/>
- <rect transform="rotate(-20 1327.76 718.667)" rx="5" id="svg_30" height="60" width="40" y="688.66667" x="1307.76187" stroke="#FFAC8D" fill="#FFAC8D"/>
- <rect transform="rotate(-25 1277 741.048)" rx="5" id="svg_31" height="60" width="40" y="711.04763" x="1256.99998" stroke="#FFAC8D" fill="#FFAC8D"/>
- <rect transform="rotate(-21 1227.07 762.25)" rx="5" id="svg_32" height="60" width="40" y="732.24993" x="1207.07155" stroke="#FFAC8D" fill="#FFAC8D"/>
- <rect transform="rotate(-13 1174.14 775.341)" rx="5" id="svg_33" height="60" width="40" y="745.34114" x="1154.13695" stroke="#FFAC8D" fill="#FFAC8D"/>
- <rect transform="rotate(-5 1121.48 780.095)" rx="5" id="svg_34" height="60" width="40" y="750.09526" x="1101.47615" stroke="#FFAC8D" fill="#FFAC8D"/>
- <rect rx="5" id="svg_35" height="60" width="40" y="749.7143" x="1049.38093" stroke="#FFAC8D" fill="#FFAC8D"/>
- <rect transform="rotate(7 1016.71 770)" rx="5" id="svg_36" height="60" width="40" y="740" x="996.71426" stroke="#FFAC8D" fill="#FFAC8D"/>
- <rect transform="rotate(14 964.619 757.81)" rx="5" id="svg_37" height="60" width="40" y="727.80954" x="944.619" stroke="#FFAC8D" fill="#FFAC8D"/>
- <rect transform="rotate(20 913.762 740.476)" rx="5" id="svg_38" height="60" width="40" y="710.4762" x="893.76186" stroke="#FFAC8D" fill="#FFAC8D"/>
- <rect transform="rotate(-20 1235.73 657.881)" rx="5" id="svg_39" height="60" width="40" y="627.88111" x="1215.72609" stroke="#FFAC8D" fill="#FFAC8D"/>
- <rect transform="rotate(-18 1190.24 673.524)" rx="5" id="svg_40" height="60" width="40" y="643.52381" x="1170.23807" stroke="#FFAC8D" fill="#FFAC8D"/>
- <rect transform="rotate(-10 1138.59 685.143)" rx="5" id="svg_41" height="60" width="40" y="655.14286" x="1118.5893" stroke="#FFAC8D" fill="#FFAC8D"/>
- <rect transform="rotate(-5 1085.76 690.095)" rx="5" id="svg_42" height="60" width="40" y="660.09525" x="1065.76187" stroke="#FFAC8D" fill="#FFAC8D"/>
- <rect transform="rotate(7 1028.14 684.286)" rx="5" id="svg_43" height="60" width="40" y="654.28571" x="1008.14283" stroke="#FFAC8D" fill="#FFAC8D"/>
- <rect transform="rotate(14 972.433 670.667)" rx="5" id="svg_44" height="60" width="40" y="640.66668" x="952.43286" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 2)" transform="rotate(-15 1467.38 767.905)" rx="5" id="svg_3" height="60" width="40" y="737.90477" x="1447.38093" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 3)" transform="rotate(-15 1413.95 779.81)" rx="5" id="svg_4" height="60" width="40" y="749.80953" x="1393.95235" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 1)" transform="rotate(-20 1516.43 751.815)" rx="5" id="svg_5" height="60" width="40" y="721.81454" x="1496.43356" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 9)" transform="rotate(-4 1105.1 876.857)" rx="5" id="svg_6" height="60" width="40" y="846.85716" x="1085.09521" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 7)" transform="rotate(-12 1214.3 864.988)" rx="5" id="svg_7" height="60" width="40" y="834.98818" x="1194.30359" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 14)" transform="rotate(20 845.762 813.238)" rx="5" id="svg_8" height="60" width="40" y="783.2381" x="825.76191" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 6)" transform="rotate(-20 1266.59 851.149)" rx="5" id="svg_9" height="60" width="40" y="821.14893" x="1246.5893" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 10)" rx="5" id="svg_10" height="60" width="40" y="845.42858" x="1027.47617" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 11)" transform="rotate(7 996.714 865.714)" rx="5" id="svg_11" height="60" width="40" y="835.71429" x="976.71426" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 4)" transform="rotate(-24 1363.63 801.06)" rx="5" id="svg_12" height="60" width="40" y="771.05975" x="1343.63085" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 5)" transform="rotate(-28 1315.94 827.269)" rx="5" id="svg_13" height="60" width="40" y="797.26882" x="1295.94007" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 8)" transform="rotate(-7 1160.05 872.798)" rx="5" id="svg_14" height="60" width="40" y="842.79771" x="1140.04758" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 16)" transform="rotate(27 742.524 763.524)" rx="5" id="svg_15" height="60" width="40" y="733.52381" x="722.52377" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 17)" transform="rotate(28 694.524 736.667)" rx="5" id="svg_16" height="60" width="40" y="706.66667" x="674.52376" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 38)" transform="rotate(40 257.4 443.439)" rx="5" id="svg_17" height="60" width="40" y="413.43862" x="237.39991" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 19)" transform="rotate(15 585.762 692.857)" rx="5" id="svg_18" height="60" width="40" y="662.85716" x="565.76188" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 12)" transform="rotate(14 946.048 850.667)" rx="5" id="svg_19" height="60" width="40" y="820.66669" x="926.04757" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 18)" transform="rotate(25 640.81 711.048)" rx="5" id="svg_20" height="60" width="40" y="681.04762" x="620.80949" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 13)" transform="rotate(20 896.619 833.333)" rx="5" id="svg_21" height="60" width="40" y="803.33335" x="876.61901" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 40)" transform="rotate(50 122.582 351.17)" rx="5" id="svg_22" height="60" width="35" y="321.1696" x="105.08185" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 15)" transform="rotate(24 791.952 788.857)" rx="5" id="svg_23" height="60" width="40" y="758.85715" x="771.95238" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 39)" transform="rotate(40 221.225 414.105)" rx="5" id="svg_24" height="60" width="40" y="384.10528" x="201.22451" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 42)" transform="rotate(60 71.6432 276.596)" rx="5" id="svg_25" height="60" width="40" y="246.5965" x="51.64324" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 41)" transform="rotate(55 90.7192 319.801)" rx="5" id="svg_26" height="60" width="40" y="289.80118" x="70.71925" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 20)" transform="rotate(-20 1489.29 673.243)" rx="5" id="svg_27" height="60" width="40" y="643.24311" x="1469.2907" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 21)" transform="rotate(-15 1440.24 686.476)" rx="5" id="svg_28" height="60" width="40" y="656.47619" x="1420.23807" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 22)" transform="rotate(-15 1386.81 702.667)" rx="5" id="svg_29" height="60" width="40" y="672.66667" x="1366.8095" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 23)" transform="rotate(-20 1327.76 718.667)" rx="5" id="svg_30" height="60" width="40" y="688.66667" x="1307.76187" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 24)" transform="rotate(-25 1277 741.048)" rx="5" id="svg_31" height="60" width="40" y="711.04763" x="1256.99998" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 25)" transform="rotate(-21 1227.07 762.25)" rx="5" id="svg_32" height="60" width="40" y="732.24993" x="1207.07155" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 26)" transform="rotate(-13 1174.14 775.341)" rx="5" id="svg_33" height="60" width="40" y="745.34114" x="1154.13695" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 27)" transform="rotate(-5 1121.48 780.095)" rx="5" id="svg_34" height="60" width="40" y="750.09526" x="1101.47615" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 28)" rx="5" id="svg_35" height="60" width="40" y="749.7143" x="1049.38093" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 29)" transform="rotate(7 1016.71 770)" rx="5" id="svg_36" height="60" width="40" y="740" x="996.71426" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 30)" transform="rotate(14 964.619 757.81)" rx="5" id="svg_37" height="60" width="40" y="727.80954" x="944.619" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 31)" transform="rotate(20 913.762 740.476)" rx="5" id="svg_38" height="60" width="40" y="710.4762" x="893.76186" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 32)" transform="rotate(-20 1235.73 657.881)" rx="5" id="svg_39" height="60" width="40" y="627.88111" x="1215.72609" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 33)" transform="rotate(-18 1190.24 673.524)" rx="5" id="svg_40" height="60" width="40" y="643.52381" x="1170.23807" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 34)" transform="rotate(-10 1138.59 685.143)" rx="5" id="svg_41" height="60" width="40" y="655.14286" x="1118.5893" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 35)" transform="rotate(-5 1085.76 690.095)" rx="5" id="svg_42" height="60" width="40" y="660.09525" x="1065.76187" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 36)" transform="rotate(7 1028.14 684.286)" rx="5" id="svg_43" height="60" width="40" y="654.28571" x="1008.14283" stroke="#FFAC8D" fill="#FFAC8D"/>
+ <rect @click="$emit('rectangleClicked', 37)" transform="rotate(14 972.433 670.667)" rx="5" id="svg_44" height="60" width="40" y="640.66668" x="952.43286" stroke="#FFAC8D" fill="#FFAC8D"/>
  
  <text transform="rotate(15 585.958 693.806)" font-weight="normal" xml:space="preserve" text-anchor="start" font-family="'Noto Sans Mono'" font-size="22" stroke-width="0" id="svg_45" y="702.30559" x="572.75696" stroke="#FFAC8D" fill="#000000">19</text>
  <text transform="rotate(25 640.235 712.694)" font-weight="normal" xml:space="preserve" text-anchor="start" font-family="'Noto Sans Mono'" font-size="22" stroke-width="0" id="svg_46" y="721.19434" x="627.0343" stroke="#FFAC8D" fill="#000000">18</text>
