a14831d0ff2f1fb46fd91a10be3d0495e73a5e24
Øystein Grande <oystgran@stud.ntnu.no>
2025-03-27 13:32:17 +0100

    Flags now showing in GuestBook component

--- campingboka/src/components/GuestBookCard.vue
+++ campingboka/src/components/GuestBookCard.vue
@@ -5,7 +5,26 @@
       <el-col :span="7" style="font-weight: bold">
         <slot name="bilnummer"></slot>
       </el-col>
-      <el-col :span="4">{{ nasjonalitet }}</el-col>
+      <el-col :span="4" style="display: flex; align-items: center; gap: 6px">
+        <img
+          v-if="countryFlag"
+          :src="countryFlag"
+          :alt="countryCode"
+          width="20"
+          height="15"
+          :title="nasjonalitet"
+        />
+      </el-col>
+      <!-- <el-col :span="4" style="display: flex; align-items: center; gap: 6px">
+        <img
+          v-if="country"
+          :src="country.flag"
+          :alt="country.name"
+          width="20"
+          height="15"
+        />
+        <span>{{ country?.name || nasjonalitet }}</span>
+      </el-col> -->
       <el-col :span="4">{{ innsjekk }} </el-col>
       <el-col :span="4">{{ utsjekk }} </el-col>
       <el-col :span="2">{{ pris }}</el-col>
@@ -13,6 +32,8 @@
   </el-card>
 </template>
 <script>
+import { countries } from "@/tools/countries";
+
 export default {
   name: "GuestBookCard",
   props: {
@@ -22,6 +43,17 @@ export default {
     innsjekk: String,
     pris: [Number, String], // Pris kan være et tall eller tom
     utsjekk: String,
+  },
+  computed: {
+    countryCode() {
+      const entry = Object.entries(countries).find(
+        ([code, data]) => data.name === this.nasjonalitet
+      );
+      return entry ? entry[0] : null;
+    },
+    countryFlag() {
+      return this.countryCode ? countries[this.countryCode].flag : null;
+    },
   },
 };
 </script>
