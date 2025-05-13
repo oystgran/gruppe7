c6a5b797b048edb914031222356040628ce69086
Øystein Grande <oystgran@stud.ntnu.no>
2025-05-13 11:17:00 +0200

    Removed unused dashboardscreen

--- campingboka/src/tools/router.js
+++ campingboka/src/tools/router.js
@@ -1,17 +1,35 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import { useAuthStore }                  from "@/stores/auth";
-import BookScreen from '@/screens/BookScreen.vue';
-import MapScreen from '@/screens/MapScreen.vue';
-import DashboardScreen from '@/screens/DashboardScreen.vue';
-import ArchiveScreen from '@/screens/ArchiveScreen.vue';
-import LoginScreen from '@/screens/LoginScreen.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import { useAuthStore } from "@/stores/auth";
+import BookScreen from "@/screens/BookScreen.vue";
+import MapScreen from "@/screens/MapScreen.vue";
+import ArchiveScreen from "@/screens/ArchiveScreen.vue";
+import LoginScreen from "@/screens/LoginScreen.vue";
 
 const routes = [
-  { path: '/', name:"Dashboard", component: DashboardScreen, meta: { requiresAuth: true } },
-  { path: "/login", name:"Login", component: LoginScreen, meta: { requiresAuth: false } },
-  { path: '/book', name:"Books",component: BookScreen , meta: { requiresAuth: true }},
-  { path: '/map', name:"Map" ,component: MapScreen, meta: { requiresAuth: true } },
-  { path: '/archive', name:"Archive", component: ArchiveScreen, meta: { requiresAuth: true } },
+  {
+    path: "/login",
+    name: "Login",
+    component: LoginScreen,
+    meta: { requiresAuth: false },
+  },
+  {
+    path: "/book",
+    name: "Books",
+    component: BookScreen,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: "/map",
+    name: "Map",
+    component: MapScreen,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: "/archive",
+    name: "Archive",
+    component: ArchiveScreen,
+    meta: { requiresAuth: true },
+  },
 ];
 
 const router = createRouter({
